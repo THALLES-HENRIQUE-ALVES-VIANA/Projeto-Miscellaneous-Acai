23a0dd67187cc9c11b90a82a8666133891ee66a7
ALiceCezar <alicecezar.estudos@gmail.com>
2024-08-22 17:23:08 -0300

    Update login.js
    Correção de bugs

--- MiscellaneousAçaí/login.js
+++ MiscellaneousAçaí/login.js
@@ -1,14 +1,18 @@
 function validacao() {
     let x = document.forms["formLogin"]["email"].value;
-    if (x == "") {
-      document.getElementById("email").style.borderColor = "red";
-      return false;
-    }
-}
-function validacao2() {
     let y = document.forms["formLogin"]["senha"].value;
-    if (y == "") {
-      document.getElementById("senha").style.borderColor = "red";
-      return false;
+    if (x == "") {
+        document.getElementById("email").style.borderColor = "red";
+        if(y == ""){
+            document.getElementById("senha").style.borderColor = "red";
+            return false;
+        }
+        return false;
+    }
+    else{
+        if(y == ""){
+            document.getElementById("senha").style.borderColor = "red";
+            return false;
+        }
     }
 }

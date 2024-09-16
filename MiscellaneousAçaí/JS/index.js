57cb23e51ec8b673c0af504a32e63d40f8c04209
THALLES HENRIQUE ALVES VIANA <thalles.henrique.thav@gmail.com>
2024-09-16 13:19:22 -0300

    organização das pastas

---
+++ MiscellaneousAçaí/JS/index.js
@@ -0,0 +1,38 @@
+var banner = document.querySelector('.carrossel');
+var banners = [
+    'imagens/img_carrossel1.png',
+    'imagens/img_carrossel2.png'
+];
+
+var bannerAtual = 0;
+var bannerAtual2 = banners.length - 1;
+
+function trocaBanner(){
+    bannerAtual++;
+    if(bannerAtual == banners.length){
+        bannerAtual = 0;
+    }
+    banner.src = banners[bannerAtual];
+    bannerAtual2 = bannerAtual;
+}
+
+function voltaBanner(){
+    bannerAtual2--;
+    if(bannerAtual2 == -1){
+        bannerAtual2 = banners.length - 1;
+    }
+    banner.src = banners[bannerAtual2];
+    bannerAtual = bannerAtual2;
+}
+
+function popup(){
+    document.querySelector(".pop-up").remove();
+}
+
+function aceito(){
+    document.querySelector(".cookies-mensagem").remove();
+}
+
+function nego(){
+    document.querySelector(".cookies-mensagem").remove();
+}

35ab71c48282b25863e9309faeff95237449bb5e
LariMartinss <larissa.santana90@etec.sp.gov.br>
2024-10-21 12:21:48 -0300

    comentários navegador.js

--- MiscellaneousAçaí/JS/navegador.js
+++ MiscellaneousAçaí/JS/navegador.js
@@ -15,23 +15,33 @@ const produtos = [
     "copo com morango",
     "copo com banana",
     "combo"
-];
+];  // define um array chamado produtos com uma lista do nosso cardápio  
+
 function pesquisar() {
+    // é chamada sempre que o usuário digitar no campo e verifica se o nome de cada produto tem o texto da busca 
+    
     let termo = document.getElementById('urlInput').value;
+    // captura oq foi inserido pelo usuário com campo de entrada id URL input
     
     let resultados = produtos.filter(item => item.toLowerCase().includes(termo));    
+     // filtra os produtos e encontra os que tem o termo digitado
     
     let resultadoDiv = document.getElementById('resultados');
+   //  onde os resultados vão ser exibidos 
     
     if (resultados.length > 0) {
-       resultados.map(item => document.getElementById(item.split(" ").join("-")).style.display = "flex");
+        // ver se tem produtos correspondentes 
+        
+       resultados.map(item => document.getElementById(item.split(" ").join("-")).style.display = "flex"); // p cada produto que for encontrado muda o estilo do elemento correspondente p ser exibido 
     } else {
         resultadoDiv.innerHTML = "<h3>Nenhum produto encontrado</h3>";
     }
-}
+} 
+// se nada for encontrado vai exibir uma mensagem mostrando que não tem resultados
 
 window.addEventListener("keydown", function (event) {
     if(event.key == "Enter"){
         pesquisar();
     }
 });
+// detectar quando apertar uma tecla, se o enter for pressionado chama a ficar pesquisar e realiza a busca 

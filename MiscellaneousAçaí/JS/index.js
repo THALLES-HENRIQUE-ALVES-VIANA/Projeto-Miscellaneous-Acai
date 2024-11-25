d1c6bd758df1bf10405fa573f19c390ad365ae32
Thalles Henrique <thalles.henrique.thav@gmail.com>
2024-11-24 23:00:12 -0300

    Adição de gráfico e mudança no index

--- MiscellaneousAçaí/JS/index.js
+++ MiscellaneousAçaí/JS/index.js
@@ -54,3 +54,54 @@ document.addEventListener("DOMContentLoaded", apagar());
 function produto(item){
     localStorage.setItem("produto", item);
 }
+
+/*
+Const ctx = seleciona o gráfico
+new Chart = cria um novo gráfico
+type, data, options são configurações do grafico, sendo:
+    type: tipo de gráfico
+    data: os dados do gráfico
+    options: configurações gerais do gráfico.
+*/
+const ctx = document.getElementById('graficoMelhorAvaliados');
+
+new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Copo simples', 'Copo com morango', 'Copo com banana', 'Combo', 'Combo com leite condensado', 'Barca', 'Vitamina de açai'],
+        datasets: [
+        {
+            label: 'Rio de Janeiro',
+            data: [3,4.8,5,4.5,5,3.5,3],
+            backgroundColor: 'rgba(200,0,100,0.2)',
+            borderWidth: 1
+        },
+        {
+            label: 'São Paulo',
+            data: [4.5,4,4,5,3,5,2],
+            backgroundColor: 'rgba(100,0,200,0.2)',
+            borderWidth: 1
+        },
+        {
+            label: 'Espirito Santo',
+            data: [3.5,5,4,4,3,2,1],
+            backgroundColor: 'rgba(200,0,0,0.2)',
+            borderWidth: 1
+        },
+        {
+            label: 'Minas Gerais',
+            data: [1,4,5,3,5,4.8,3],
+            backgroundColor: 'rgba(0,0,200,0.5)',
+            borderWidth: 1
+        }
+    ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
